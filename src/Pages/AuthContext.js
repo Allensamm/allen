63338d65efd183e3a3672072d96f477012a1ea9c
allen samuel <allensamuel569@gmail.com>
2023-12-31 18:21:29 +0100

    changed the authorization settings

---
+++ src/Pages/AuthContext.js
@@ -0,0 +1,71 @@
+// authContext.js
+import { createContext, useContext, useState, useEffect } from 'react';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children, apiURL }) => {
+  const [currentUser, setCurrentUser] = useState(() => {
+    const storedUser = localStorage.getItem('currentUser');
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const userResponse = await fetch(`${apiURL}/api/user`, { credentials: 'include' });
+
+        if (userResponse.ok) {
+          const user = await userResponse.json();
+          console.log(user);
+          // Check if user data is valid before setting currentUser
+          if (user && user.id) {
+            setCurrentUser(true);
+          } else {
+            setCurrentUser(false);
+          }
+        } else {
+          setCurrentUser(false);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+        setCurrentUser(false);
+      }
+    };
+
+    if (!currentUser) {
+      fetchUser();
+    }
+  }, [currentUser, apiURL]);
+
+  const login = () => {
+    setCurrentUser(true);
+    localStorage.setItem('currentUser', JSON.stringify(true));
+  };
+
+  const logout = async () => {
+    try {
+      const response = await fetch(`${apiURL}/api/logout/`, {
+        method: 'POST',
+        credentials: 'include',
+      });
+
+      if (response.ok) {
+        setCurrentUser(false);
+        localStorage.removeItem('currentUser');
+        window.location.href = '/home';
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ currentUser, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
