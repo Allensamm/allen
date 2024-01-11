3c2c821b39f74495dda850b9bbcdd14222f3ab09
allen samuel <allensamuel569@gmail.com>
2024-01-11 22:06:12 +0100

    updated some logics

--- src/Pages/Home/Blog.jsx
+++ src/Pages/Home/Blog.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import './Blog.css'
+import "./Blog.css";
 import { Link, useParams } from "react-router-dom";
 
 function PostList() {
@@ -30,31 +30,37 @@ function PostList() {
   }, [category]);
 
   return (
-    <div className="main">
-      <h2>Blog Posts</h2>
-      <ul>
-        {posts.map((post) => (
-          <div className="posts" key={post.id}>
-            <h1 className="posttitle">{post.projectname}</h1>
-            <p className="content">{post.Description}</p>
+    <div className="main m-8">
+      {posts.map((post) => (
+        <div className="posts" key={post.id}>
+          <div style={{border:'1px solid #78e0ea'}} className=" mb-10 p-10 rounded-md">
+            <h1 className="posttitle font-semibold text-5xl text-[#78e0ea]">
+              {post.projectname}
+            </h1>
+            <p className="content text-base text-white leading-loose mb-8">
+              {post.Description}
+            </p>
 
-            {post.anyfile && (
-              <div>
-                <video width="320" height="240" controls>
-                  <source src={post.anyfile} type="video/mp4" />
-                  Your browser does not support the video tag.
-                </video>
-              </div>
-            )}
+            <div className="mb-10">
+              {typeof post.anyfile === "string" &&
+                post.anyfile.endsWith(".png") && (
+                  <img src={post.anyfile} alt="Image" />
+                )}
+              {typeof post.anyfile === "string" &&
+                post.anyfile.endsWith(".mp4") && (
+                  <video controls width="320" height="240">
+                    <source src={post.anyfile} type="video/mp4" />
+                    Your browser does not support the video tag.
+                  </video>
+                )}
+            </div>
 
-            <Link to={post.linktoproject}>
-              {post.linktoproject}
-            </Link>
+            <Link className="bg-[#ffc107] link p-4 rounded-sm" to={post.linktoproject}>view in github</Link>
             <div dangerouslySetInnerHTML={{ __html: post.largecontent }} />
             <img src={post.image} alt="" />
           </div>
-        ))}
-      </ul>
+        </div>
+      ))}
     </div>
   );
 }
